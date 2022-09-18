1b1e80ab781c1c21aa85efbdaa2e6ab5c13f702f
wajidbaiguk <110336747+wajidbaiguk@users.noreply.github.com>
2022-09-18 21:01:18 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -115,13 +115,13 @@ var score=0;
 //   red.scale = 0.1;
 // }
 
-// function redBalloon() {
-//   var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
-//   red.addImage(red_balloonImage);
-//   red.velocityX = 3;
-//   red.lifetime = 150;
-//   red.scale = 0.1;
-// }
+function redBalloon() {
+  var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+}
 
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
